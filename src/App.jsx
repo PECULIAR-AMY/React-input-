003ccabf4dfe6299a5a88c7624ba407811563e25
unknown <emmanuelpeculiar645@gmail.com>
2024-11-04 14:26:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
+import Input from './components/Input.jsx';
+import Button from './components/Button.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
